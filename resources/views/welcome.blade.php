88a43fd4c218d314af606afab65fa0e164aeb41d
Chau1910 <chaub1507205@student.ctu.edu.vn>
2018-10-25 19:09:23 +0700

    Hieu chinh ten

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Tran Thi Cam Chau
                 </div>
 
                 <div class="links">
