c824f5b94d48da1f00839ba5e53beb16df4c56d7
Chau1910 <chaub1507205@student.ctu.edu.vn>
2018-11-01 14:24:47 +0700

    New: updates 14 table

---
+++ database/migrations/2018_11_01_065329_create_hinhanh_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHinhanhTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hinhanh', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->bigInteger('sp_ma')
+                ->comment('Ma san pham');
+            $table->unsignedInteger('ha_stt')
+                ->default('1');
+            $table->string('ha_ten',150)
+                ->comment('Ten hinh anh');
+            
+            $table->foreign('sp_ma')
+                ->references('sp_ma')
+                ->on('sanpham');
+            
+            $table->primary(['ha_stt', 'sp_ma']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hinhanh');
+    }
+}
