ae13e7267e976035d1b0a4398d209d1679047853
Chau1910 <chaub1507205@student.ctu.edu.vn>
2018-11-03 19:19:24 +0700

    create Model

---
+++ app/HinhAnh.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HinhAnh extends Model
+{
+    public $timestamp = false;
+
+    protected $table       = 'hinhanh';
+    protected $fillable    = ['ha_ten', 'sp_ma'];
+    protected $guarded       = ['ha_stt'];
+
+    protected $primaryKey  = ['sp_ma, hs_stt'];
+    public $incrementing = false;
+
+    public function hinhsanpham(){
+        return $this->belongsTo('App\sanpham','sp_ma','sp_ma');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Loai;
+
+Route::get('/danhsachloai',function(){
+    //Eloquent Model de lay du lieu
+    //$ds_loai = Loai::all(); //select * from loai
+    //$json = json_encode($ds_loai);
+    //return $json;
+
+    //Query Builder
+    $ds_loai = DB::table('loai')->get();
+    $json = json_encode($ds_loai);
+    return $json;
+});
+use App\ChuDe;
+
+Route::get('/danhsachchude',function(){
+    //$ds_cd = ChuDe::all();
+    //$json = json_encode($ds_cd);
+    //return $json;
+
+    //Query Builder
+    $ds_cd = DB::table('chude')->get();
+    $json = json_encode($ds_cd);
+    return $json;
+
+});
