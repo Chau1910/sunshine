1413bf59eb5dab2206302948cc7026925edb1afe
Chau1910 <chaub1507205@student.ctu.edu.vn>
2018-11-22 15:04:06 +0700

    upload file hinh anh

--- app/Http/Controllers/SanPhamController.php
+++ app/Http/Controllers/SanPhamController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SanPham;
+use Session;
 
 class SanPhamController extends Controller
 {
@@ -21,6 +22,21 @@ class SanPhamController extends Controller
             ->with('danhsachloai', $ds_loai);
     }
     public function store(Request $request){
-        
+        $sp = new SanPham();
+        $sp->sp_ten = $request->sp_ten;
+
+        if($request->hasFile('sp_hinh')){
+            $file = $request->sp_hinh;
+            //luu ten hinh vao column sp_hinh
+            $sp->sp_hinh = $file->getClientOriginalName();
+
+            //Chep file vao thu muc "photos"
+            $file->storeAs('photos', $file->getClientOriginalName());
+        }
+
+        $sp->save();
+
+        Session::flash('alert-info', 'Them moi thanh cong!!!!');
+        return redirect()->route('danhsachsanpham.index');
     }
 }
--- resources/views/sanpham/create.blade.php
+++ resources/views/sanpham/create.blade.php
@@ -8,29 +8,32 @@
 
 <h2>Them moi loai san pham</h2>
 
-<form  method="post" action="{{ route('danhsachloai.store') }}" enctype="multipart/form-data">
+<form  method="post" action="{{ route('danhsachsanpham.store') }}" enctype="multipart/form-data">
     {{ csrf_field() }}
-<div class="form-group">
-    <label for="l_ma">Loai san pham</label>
-        <input type="text" class="form-control" id="l_ten" name="l_ten" placeholder="Nhap ten loai">
-</div>  
-<div class="form-group">
-    <label for="l_taoMoi">Nhap ngay tao moi</label>
-        <input type="text" class="form-control" id="l_taoMoi" name="l_taoMoi" placeholder="Nhap ngay tao moi">
-</div>
-<div class="form-group">
-    <label for="l_capNhat">Nhap ngay cap nhat</label>
-        <input type="text" class="form-control" id="l_capNhat" name="l_capNhat" placeholder="Nhap ngay cap nhat">
-</div>
-<div class="form-group">
-    <label for="l_ma">Loai san pham</label>
-    <select name="l_ma">
-        @foreach($danhsachloai as $loai)
-        <option value="{{ $loai -> l_ma }}">Khoa</option>
-        @endforeach
-    </select>
-        
-</div>
-<button type="submit" class="btn btn-primary">Add</button>     
+    <div class="form-group">
+        <label for="l_ma">Loai san pham</label>
+        <select name="l_ma">
+            @foreach($danhsachloai as $loai)
+                <option value="{{ $loai -> l_ma }}">{{ $loai->l_ten }}</option>
+            @endforeach
+        </select>        
+    </div>
+
+    <div class="form-group">
+        <label for="sp_ten">Ten san pham</label>
+            <input type="text" class="form-control" id="sp_ten" name="sp_ten" placeholder="">
+    </div>  
+
+    <div class="form-group">
+        <label for="sp_giaGoc">Gia goc</label>
+            <input type="text" class="form-control" id="sp_giaGoc" name="sp_giaGoc" placeholder="Nhap gia goc">
+    </div>
+
+    <div class="form-group">
+        <label for="sp_hinh">Hinh dai dien</label>
+            <input type="file" name="sp_hinh">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Add</button>     
 </form>                      
 @endsection
